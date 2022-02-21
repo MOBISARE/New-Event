ba08de946b7f4281a4fedd3bfc81815754e61a38
Florian Mathieu <mathieuflo88@gmail.com>
2022-02-21 22:43:43 +0100

    Ajout tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   render() {
     return(
       <div>
-        <h1>App</h1>
+        <h1 className='text-cyan-500'>App</h1>
         <Test/>
       </div>
     );
