3f70296d4b217dcfd030c0dfa82be9a10b37f0bf
Florian Mathieu <mathieuflo88@gmail.com>
2022-02-24 08:21:31 +0100

    Début homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,16 @@ class Home extends React.Component {
     render(){
         return(
             <div>
-                <h1>HOME</h1>
+                <div className='container fixed h-16 max-w-full bg-white shadow-md'> Test </div>
+
+                <div className='py-24 bg-gradient-to-r from-purple_gradient to-blue h-screen'>
+                    <h1 className='text-9xl text-gray font-roboto'>newEvent</h1>
+                </div>
+
+                <div className='py-24 bg-gray h-screen'>
+                    <h1 >HOME</h1>
+                </div>
+                
             </div>
         );
     }
