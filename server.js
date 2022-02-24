2c0443686628e3d8d612a59ec9bf33990448d7f3
Maxime <monchemax540@gmail.com>
2022-02-24 08:22:23 +0100

    .

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api/test', (req, res) => {
-    const test = { id : "oui"};
-
-    res.json(test);
-})
-
 const port = 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+//routage
+app.get('/api/test', (req, res) => {
+    const test = { id: "oui" };
+
+    res.json(test);
+})
