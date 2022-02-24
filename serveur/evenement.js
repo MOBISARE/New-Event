a95d4ad3b45a115bf3920bbd55d5c1c2c928acbb
Maxime <monchemax540@gmail.com>
2022-02-24 10:10:06 +0100

    ajout db.js bdd.json

---
+++ serveur/evenement.js
@@ -0,0 +1,8 @@
+function getEvenementModification(id) {
+    //recupere les donnees de l evenement
+    return { "id": id }
+}
+
+
+
+module.exports.getEvenementModification = getEvenementModification
