f0b8e8d2f54779a3564007154579cad6a2d9ab12
Florian Mathieu <mathieuflo88@gmail.com>
2022-02-24 10:20:55 +0100

    Modif front homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,13 +4,25 @@ class Home extends React.Component {
     render(){
         return(
             <div>
-                <div className='container fixed h-16 max-w-full bg-white shadow-md'> Test </div>
+                <div className='container fixed h-16 p-2 max-w-full flex justify-between bg-white shadow-md'>
+                    <img className='' src='/images/Logo.png'></img>
 
-                <div className='py-24 bg-gradient-to-r from-purple_gradient to-blue h-screen'>
-                    <h1 className='text-9xl text-gray font-roboto'>newEvent</h1>
+                    <div className='flex gap-5 items-center'>
+                        <div>TEST</div>
+                        <div>TEST</div>
+                    </div>
+
+                
                 </div>
 
-                <div className='py-24 bg-gray h-screen'>
+                <div className='py-24 bg-gradient-to-r from-purple_gradient to-blue h-screen w-full'>
+                    <div className='p-11 w-fit max-m absolute'>
+                        <h1 className='text-9xl leading-[0.6] text-gray font-logo font-black left-0'>newEvent</h1>
+                        <h3 className='text-4xl text-gray text-right font-roboto font-medium right-1'>créateur d'événement</h3>
+                    </div>
+                </div>
+
+                <div className='py-24 bg-gray h-screen w-full'>
                     <h1 >HOME</h1>
                 </div>
                 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,13 +12,14 @@ module.exports = {
       'gray': '#f5f7fa'
     },
     fontFamily: {
-      'logo' : ['Baloo Bhai'],
+      'logo' : ['Baloo\\ Bhai\\ 2'],
       'sans'	: [ 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji' ],
       'serif': [ 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
       'mono' :	[ 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier-New', 'monospace' ],
       'roboto': ['Roboto', 'Consolas']
     },
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
