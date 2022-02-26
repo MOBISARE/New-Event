fb1abd128495bf6fc1797caf8e32aab576c9f4f2
Florian Mathieu <mathieuflo88@gmail.com>
2022-02-26 11:17:16 +0100

    Ajout composants pour les forms

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -6,12 +6,13 @@ class Button extends React.Component {
     static defaultProps = {
         text_class: '',
         bg_class: '',
-        to: '#'
+        to: '#',
+        className: ''
     }
 
     render(){
         return(
-            <Link to={this.props.to}>
+            <Link to={this.props.to} className={this.props.className}>
                 <div className={'border-solid border rounded-full border-transparentgray px-8 py-2 flex items-center justify-center bg-darkgray ' + this.props.bg_class}>
                     <div className= {'text-xl text-gray ' + this.props.text_class}>
                         {this.props.children}
--- client/src/components/InputField.js
+++ client/src/components/InputField.js
@@ -4,14 +4,15 @@ class InputField extends React.Component {
 
     static defaultProps = {
         id:'field',
-        className:''
+        className:'',
+        type: 'text'
     }
 
     render(){
         return(
-            <div>
+            <div className={this.props.className}>
                 <label htmlFor={this.props.id} className='block leading-3 ml-4 text-lg z-0'>{this.props.children}</label>
-                <input type='text' id={this.props.id} name={this.props.id} className={'rounded-full border-transparentgray z-10 ' + this.props.className}></input>
+                <input type={this.props.type} id={this.props.id} name={this.props.id} className='rounded-full border-transparentgray z-10 w-full'></input>
             </div>
         );
     }
