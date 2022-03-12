59ff45a79495bff1e6cb9cffb4fcee1bd680b72b
Florian Mathieu <mathieuflo88@gmail.com>
2022-03-12 16:40:11 +0100

    Ajout vue participants & propriétaire

--- client/src/components/Event/ParticipantViewer.js
+++ client/src/components/Event/ParticipantViewer.js
@@ -1,11 +1,12 @@
+import axios from 'axios';
 import React, { createRef } from 'react';
 import { Link } from 'react-router-dom';
-
 
 class ParticipantViewer extends React.Component {
 
     static defaultProps = {
-        button: null
+        button: null,
+        editable: false
     }
 
     constructor(props){
@@ -27,30 +28,81 @@ class ParticipantViewer extends React.Component {
             window.removeEventListener('click', this.checkClickOutside);
         }
     }
+
+    componentDidMount = () => {
+        axios({
+            method:"get",
+            url:"/api/getParticipants/:id",
+            data: {
+            }
+        })
+        .then((res) => {
+            console.log(res);
+            window.location = "/";
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
     
     componentWillUnmount = () => {
         window.removeEventListener('click', this.checkClickOutside);
     }
 
+    showUser = (elem, i) => {
+        return (
+            <div key={i} className='w-full px-3 flex hover:bg-selected-gray items-stretch justify-between'>
+                <Link to={'/profil/' + elem.id} className='flex py-2 gap-2 items-center grow'>
+                    <img className='rounded-full h-6 w-6 bg-green flex-none' src={this.props.imgUrl} />
+                    <div className='text-sm whitespace-nowrap'>{elem.firstname + " " + elem.name}</div>
+                    <div className='text-sm text-darkergray font-light text-right flex-1'>{elem.isYou ? 'Vous' : elem.owner ? 'Propriétaire' : ''}</div>
+                </Link>
+                
+                {this.props.editable && !elem.owner ? <div className='bg-red-600 text-white text-sm px-2 flex items-center hover:shadow-inner hover:bg-red-800 cursor-pointer' onClick={() => this.kick(i)}>Expulser</div> : <></>}
+            </div>
+        );
+    }
+
+    kick = (i) => {
+        console.log("kick " + i)
+    }
+
     render(){
-        var dropDownElems = [
+        var participants = [
+            {id: 1, firstname: "Prénom", name: "Nom", owner: true},
+            {id: 2, firstname: "Prénom2fff", name: "Nom2", isYou: true},
+            {id: 3, firstname: "Prénom2", name: "Nom2"},
+            {id: 4, firstname: "Prénom2", name: "Nom2"},
+            {id: 5, firstname: "Prénom2", name: "Nom2"},
+            {id: 6, firstname: "Prénom2", name: "Nom2"},
+            {id: 7, firstname: "Prénom2", name: "Nom2"},
+            {id: 8, firstname: "Prénom2", name: "Nom2"},
+            {id: 9, firstname: "Prénom2", name: "Nom2"},
+            {id: 10, firstname: "Prénom2", name: "Nom2"},
+            {id: 11, firstname: "Prénom2", name: "Nom2"},
+            {id: 12, firstname: "Prénom2", name: "Nom2"},
         ]
 
         return(
-            <div id='dropdown-account' className={'text-left bg-white absolute right-0 drop-shadow-sm border rounded-md border-transparentgray flex flex-col gap-1 py-1 min-w-[200px] ' + (this.state.active? '': 'hidden')} ref={this.DOM_element}>
+            <div id='dropdown-account' className={'text-left bg-white absolute right-0 drop-shadow-sm border rounded-md border-transparentgray flex flex-col gap-1 py-1 min-w-[200px] max-h-64 w-fit ' + (this.state.active? '': 'hidden')} ref={this.DOM_element}>
                 <div className='inline-block px-4 whitespace-nowrap py-1 text-md'>Participants</div>
                 <div className='flex items-center border-y border-transparentgray px-1'>
                     <span className="material-icons text-transparentgray">
                         search
                     </span>
-                    <input className='flex-grow placeholder:text-transparentgray focus-visible:outline-none p-1' placeholder='Rechercher des membres' />
+                    <input className='flex-grow placeholder:text-transparentgray text-sm focus-visible:outline-none px-1 py-2' placeholder='Rechercher des membres' />
                 </div>
-                <div className='flex flex-col gap-1'>
-                    <div className='flex w-full px-3 py-1 gap-2 items-center hover:bg-selected-gray'>
-                        <img className='rounded-full h-6 w-6 bg-green' src={this.props.imgUrl} />
-                        <span className='text-sm'>Prénom Nom</span>
-                    </div>
 
+                
+
+                <div className='overflow-y-scroll'>
+                    <div className='flex flex-col gap-1'>
+                        {
+                        participants.map((elem, index) => {
+                            return this.showUser(elem, index)
+                        })
+                        }
+                    </div>
                 </div>
             </div>
         );
