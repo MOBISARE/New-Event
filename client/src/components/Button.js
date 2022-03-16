04c0fb8aefff51c548f75d827085daba00c6ec44
Florian Mathieu <mathieuflo88@gmail.com>
2022-03-16 11:06:30 +0100

    Modif button

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -5,12 +5,13 @@ class Button extends React.Component {
 
     static defaultProps = {
         className: '',
-        text_class: ''
+        text_class: '',
+        onClick: undefined
     }
 
     render(){
         return(
-            <div className={'border-solid border rounded-full border-transparentgray px-8 py-2 flex items-center justify-center bg-darkgray transition hover:scale-105 ' + this.props.className}>
+            <div className={'border-solid border rounded-full border-transparentgray px-8 py-2 flex items-center justify-center bg-darkgray transition hover:scale-105 cursor-pointer ' + this.props.className} onClick={this.props.onClick}>
                 <div className= {'text-xl text-gray ' + this.props.text_class}>
                     {this.props.children}
                 </div>
